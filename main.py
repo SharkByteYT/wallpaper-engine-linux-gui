d27b784915e29e7394b0d362990b3fc426d69451
SharkByteYT <161545488+SharkByteYT@users.noreply.github.com>
2025-11-16 16:28:43 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -236,7 +236,6 @@ class CliFrontend(Gtk.Application):
                         continue
 
                 # Render the preview image
-              
                 if img_path.lower().endswith(".gif"):
                     loader = GdkPixbuf.PixbufAnimation.new_from_file(img_path)
                     pixbuf = loader.get_static_image()
@@ -246,16 +245,16 @@ class CliFrontend(Gtk.Application):
                     new_height = max(1, int(height * scale_factor))
                     scaled_pixbuf = pixbuf.scale_simple(target_width, new_height, GdkPixbuf.InterpType.BILINEAR)
                     image = Gtk.Image.new_from_pixbuf(scaled_pixbuf)
-                    image.set_size_request(width, new_height)
+                    image.set_size_request(target_width, new_height)
                 else:
                     pixbuf = GdkPixbuf.Pixbuf.new_from_file(img_path)
                     width = pixbuf.get_width()
                     height = pixbuf.get_height()
                     scale_factor = target_width / width
                     new_height = max(1, int(height * scale_factor))
-                    scaled_pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(img_path, width=width, height=new_height, preserve_aspect_ratio=True)
+                    scaled_pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(img_path, width=target_width, height=new_height, preserve_aspect_ratio=True)
                     image = Gtk.Image.new_from_pixbuf(scaled_pixbuf)
-                    image.set_size_request(width, new_height)
+                    image.set_size_request(target_width, new_height)
 
                 button = Gtk.Button()
                 button.set_child(image)
@@ -349,7 +348,7 @@ class CliFrontend(Gtk.Application):
                 return
 
             # Render the preview image
-            target_width = 1000
+            target_width = 200
             if img_path.lower().endswith(".gif"):
                 loader = GdkPixbuf.PixbufAnimation.new_from_file(img_path)
                 pixbuf = loader.get_static_image()
@@ -359,14 +358,14 @@ class CliFrontend(Gtk.Application):
                 new_height = max(1, int(height * scale_factor))
                 scaled_pixbuf = pixbuf.scale_simple(target_width, new_height, GdkPixbuf.InterpType.BILINEAR)
                 self.selected_image_preview.set_from_pixbuf(scaled_pixbuf)
-                self.selected_image_preview.set_size_request(twidth, new_height)
+                self.selected_image_preview.set_size_request(target_width, new_height)
             else:
                 pixbuf = GdkPixbuf.Pixbuf.new_from_file(img_path)
                 width = pixbuf.get_width()
                 height = pixbuf.get_height()
                 scale_factor = target_width / width
                 new_height = max(1, int(height * scale_factor))
-                scaled_pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(img_path, width, height=new_height, preserve_aspect_ratio=True)
+                scaled_pixbuf = GdkPixbuf.Pixbuf.new_from_file_at_scale(img_path, width=target_width, height=new_height, preserve_aspect_ratio=True)
                 self.selected_image_preview.set_from_pixbuf(scaled_pixbuf)
                 self.selected_image_preview.set_size_request(target_width, new_height)
         except Exception:
